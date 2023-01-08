91dce96bb85fc92125c9fe3a6db4206adacef24d
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-01-09 02:24:54 +0530

    .env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const admin = require("../models/admin/admin");
 const auth = async (req, res, next) => {
   try {
     const token = req.cookies.jwt_token;
-    const decode = await jwt.verify(token, "thisismynewcourse");
+    const decode = await jwt.verify(token, process.env.admin_jwt);
     const user = await admin.findOne({
       _id: decode._id,
       "tokens.token": token,
